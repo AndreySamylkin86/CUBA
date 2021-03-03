20b7bcbe2b08e869ece0634f530c986b42aaa367
andrey <sveshennik@ya.ru>
2021-03-03 14:50:09 +0300

    Все пункты выполнены.

--- customers/modules/web/src/com/company/customers/web/screens/order/OrderEdit.java
+++ customers/modules/web/src/com/company/customers/web/screens/order/OrderEdit.java
@@ -1,11 +1,8 @@
 package com.company.customers.web.screens.order;
 
-import com.company.customers.entity.Customer;
-import com.company.customers.entity.RegularCustomerCard;
+import com.company.customers.service.DiscountService;
 import com.haulmont.cuba.core.global.DataManager;
-import com.haulmont.cuba.gui.Dialogs;
 import com.haulmont.cuba.gui.components.Button;
-import com.haulmont.cuba.gui.components.HasValue;
 import com.haulmont.cuba.gui.components.TextField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.customers.entity.Order;
@@ -18,20 +15,19 @@ import javax.inject.Inject;
 @LoadDataBeforeShow
 public class OrderEdit extends StandardEditor<Order> {
 
+    @Inject
+    private DiscountService discountService;
+
+    @Inject
+    private DataManager dataManager;
 
 
-
-
-
-
-
+    @Inject
+    private TextField<Long> orderNumberField;
 
     @Subscribe("commitAndCloseBtn")
     public void onCommitAndCloseBtnClick(Button.ClickEvent event) {
-
-
-
+        long orderNumber = Long.parseLong(orderNumberField.getRawValue());
+        discountService.discount(orderNumber);
     }
-
-
 }
